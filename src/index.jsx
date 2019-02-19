321e776ce8999bcb462c7bea54ca5e443b886843
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2019-02-19 00:11:32 -0500

    Setting up React App for Github Pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 /* global module */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 
